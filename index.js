a919ccd2457832b0985db1d8a02cea012aaf9a52
freyappsx <andresfreyapps@gmail.com>
2025-02-02 15:54:29 +0000

    Agregamos pacage.json

---
+++ index.js
@@ -0,0 +1,24 @@
+/*const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hola desde Node.js backend');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+*/
+const http = require("http")
+
+function requestController() {
+    // Logica de nuestra funcion
+    console.log("Hola mundo")
+}
+
+
+// Configurar nuestro servidor
+const server = http.createServer(requestController)
+
+server.listen(4000)
